237f12fc50180f93504ade055f86fc560dd5d2de
kapilsolanki100 <kapilsolanki971942@gmail.com>
2025-11-17 13:13:02 +0530

    Update Firebase configuration with project details

--- js/firebase.js
+++ js/firebase.js
@@ -7,13 +7,16 @@ Replace these placeholders with your Firebase project config.
 Make sure Firestore is enabled in your Firebase console.
 */
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyD_E4uKPS_0uxmx1wMTm3jGikBgf7HwiYY",
+    authDomain: "campusbites-4e430.firebaseapp.com",
+    projectId: "campusbites-4e430",
+    storageBucket: "campusbites-4e430.firebasestorage.app",
+    messagingSenderId: "897068032498",
+    appId: "1:897068032498:web:7a28767d0c8c7b85610b6b",
+    measurementId: "G-NFEK422PTC"
+  };
+
+
 
 if (firebaseConfig.apiKey === 'YOUR_API_KEY') {
   console.warn('Please replace firebaseConfig placeholders in js/firebase.js with your project values.');
